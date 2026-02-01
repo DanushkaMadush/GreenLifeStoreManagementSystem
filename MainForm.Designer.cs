d20ff29c8784a61aed70763631f3c2dde6bd29db
danushka <danmadushzxc@gmail.com>
2026-02-01 20:01:50 +0530

    change interface

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,9 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            this.panelSidebar = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnDashboard = new System.Windows.Forms.Button();
             this.btnHome = new System.Windows.Forms.Button();
             this.btnReports = new System.Windows.Forms.Button();
@@ -46,44 +43,11 @@
             this.panelHeader = new System.Windows.Forms.Panel();
             this.btnClose = new System.Windows.Forms.Button();
             this.panelMain = new System.Windows.Forms.Panel();
-            this.panelSidebar.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panelHeader.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panelSidebar
-            // 
-            this.panelSidebar.BackColor = System.Drawing.Color.DimGray;
-            this.panelSidebar.Controls.Add(this.pictureBox1);
-            this.panelSidebar.Controls.Add(this.btnDashboard);
-            this.panelSidebar.Controls.Add(this.btnHome);
-            this.panelSidebar.Controls.Add(this.btnReports);
-            this.panelSidebar.Controls.Add(this.btnBrowseProducts);
-            this.panelSidebar.Controls.Add(this.btnManageOrders);
-            this.panelSidebar.Controls.Add(this.btnCart);
-            this.panelSidebar.Controls.Add(this.btnManageCustomers);
-            this.panelSidebar.Controls.Add(this.btnMyOrders);
-            this.panelSidebar.Controls.Add(this.btnManageProducts);
-            this.panelSidebar.Controls.Add(this.btnReviews);
-            this.panelSidebar.Controls.Add(this.btnLogout);
-            this.panelSidebar.Controls.Add(this.btnProfile);
-            this.panelSidebar.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelSidebar.Location = new System.Drawing.Point(0, 50);
-            this.panelSidebar.Name = "panelSidebar";
-            this.panelSidebar.Size = new System.Drawing.Size(200, 400);
-            this.panelSidebar.TabIndex = 0;
-            this.panelSidebar.UseWaitCursor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(44, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(115, 71);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.UseWaitCursor = true;
             // 
             // btnDashboard
             // 
@@ -94,9 +58,9 @@
             this.btnDashboard.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDashboard.ForeColor = System.Drawing.Color.White;
             this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDashboard.Location = new System.Drawing.Point(0, 315);
+            this.btnDashboard.Location = new System.Drawing.Point(3, 423);
             this.btnDashboard.Name = "btnDashboard";
-            this.btnDashboard.Size = new System.Drawing.Size(200, 30);
+            this.btnDashboard.Size = new System.Drawing.Size(200, 36);
             this.btnDashboard.TabIndex = 11;
             this.btnDashboard.Text = "Dashboard";
             this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -107,14 +71,15 @@
             // 
             this.btnHome.BackColor = System.Drawing.Color.DimGray;
             this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnHome.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
             this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnHome.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHome.ForeColor = System.Drawing.Color.White;
             this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnHome.Location = new System.Drawing.Point(0, 83);
+            this.btnHome.Location = new System.Drawing.Point(3, 3);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(200, 30);
+            this.btnHome.Size = new System.Drawing.Size(200, 36);
             this.btnHome.TabIndex = 0;
             this.btnHome.Text = "Home";
             this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -130,9 +95,9 @@
             this.btnReports.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReports.ForeColor = System.Drawing.Color.White;
             this.btnReports.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnReports.Location = new System.Drawing.Point(0, 288);
+            this.btnReports.Location = new System.Drawing.Point(3, 381);
             this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(200, 30);
+            this.btnReports.Size = new System.Drawing.Size(200, 36);
             this.btnReports.TabIndex = 10;
             this.btnReports.Text = "Reports";
             this.btnReports.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -148,9 +113,9 @@
             this.btnBrowseProducts.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBrowseProducts.ForeColor = System.Drawing.Color.White;
             this.btnBrowseProducts.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnBrowseProducts.Location = new System.Drawing.Point(0, 109);
+            this.btnBrowseProducts.Location = new System.Drawing.Point(3, 339);
             this.btnBrowseProducts.Name = "btnBrowseProducts";
-            this.btnBrowseProducts.Size = new System.Drawing.Size(200, 30);
+            this.btnBrowseProducts.Size = new System.Drawing.Size(200, 36);
             this.btnBrowseProducts.TabIndex = 1;
             this.btnBrowseProducts.Text = "Browse Products";
             this.btnBrowseProducts.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -166,9 +131,9 @@
             this.btnManageOrders.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnManageOrders.ForeColor = System.Drawing.Color.White;
             this.btnManageOrders.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnManageOrders.Location = new System.Drawing.Point(0, 261);
+            this.btnManageOrders.Location = new System.Drawing.Point(3, 297);
             this.btnManageOrders.Name = "btnManageOrders";
-            this.btnManageOrders.Size = new System.Drawing.Size(200, 30);
+            this.btnManageOrders.Size = new System.Drawing.Size(200, 36);
             this.btnManageOrders.TabIndex = 9;
             this.btnManageOrders.Text = "Manage Orders";
             this.btnManageOrders.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -184,9 +149,9 @@
             this.btnCart.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCart.ForeColor = System.Drawing.Color.White;
             this.btnCart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCart.Location = new System.Drawing.Point(0, 134);
+            this.btnCart.Location = new System.Drawing.Point(3, 255);
             this.btnCart.Name = "btnCart";
-            this.btnCart.Size = new System.Drawing.Size(200, 30);
+            this.btnCart.Size = new System.Drawing.Size(200, 36);
             this.btnCart.TabIndex = 2;
             this.btnCart.Text = "Cart";
             this.btnCart.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -202,9 +167,9 @@
             this.btnManageCustomers.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnManageCustomers.ForeColor = System.Drawing.Color.White;
             this.btnManageCustomers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnManageCustomers.Location = new System.Drawing.Point(0, 236);
+            this.btnManageCustomers.Location = new System.Drawing.Point(3, 213);
             this.btnManageCustomers.Name = "btnManageCustomers";
-            this.btnManageCustomers.Size = new System.Drawing.Size(200, 30);
+            this.btnManageCustomers.Size = new System.Drawing.Size(200, 36);
             this.btnManageCustomers.TabIndex = 8;
             this.btnManageCustomers.Text = "Manage Customers";
             this.btnManageCustomers.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -220,9 +185,9 @@
             this.btnMyOrders.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMyOrders.ForeColor = System.Drawing.Color.White;
             this.btnMyOrders.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnMyOrders.Location = new System.Drawing.Point(0, 159);
+            this.btnMyOrders.Location = new System.Drawing.Point(3, 171);
             this.btnMyOrders.Name = "btnMyOrders";
-            this.btnMyOrders.Size = new System.Drawing.Size(200, 30);
+            this.btnMyOrders.Size = new System.Drawing.Size(200, 36);
             this.btnMyOrders.TabIndex = 3;
             this.btnMyOrders.Text = "My Orders";
             this.btnMyOrders.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -238,9 +203,9 @@
             this.btnManageProducts.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnManageProducts.ForeColor = System.Drawing.Color.White;
             this.btnManageProducts.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnManageProducts.Location = new System.Drawing.Point(0, 211);
+            this.btnManageProducts.Location = new System.Drawing.Point(3, 129);
             this.btnManageProducts.Name = "btnManageProducts";
-            this.btnManageProducts.Size = new System.Drawing.Size(200, 30);
+            this.btnManageProducts.Size = new System.Drawing.Size(200, 36);
             this.btnManageProducts.TabIndex = 7;
             this.btnManageProducts.Text = "Manage Products";
             this.btnManageProducts.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -256,9 +221,9 @@
             this.btnReviews.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReviews.ForeColor = System.Drawing.Color.White;
             this.btnReviews.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnReviews.Location = new System.Drawing.Point(0, 186);
+            this.btnReviews.Location = new System.Drawing.Point(3, 87);
             this.btnReviews.Name = "btnReviews";
-            this.btnReviews.Size = new System.Drawing.Size(200, 30);
+            this.btnReviews.Size = new System.Drawing.Size(200, 36);
             this.btnReviews.TabIndex = 4;
             this.btnReviews.Text = "Review and Ratings";
             this.btnReviews.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -274,9 +239,9 @@
             this.btnLogout.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogout.ForeColor = System.Drawing.Color.White;
             this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnLogout.Location = new System.Drawing.Point(0, 369);
+            this.btnLogout.Location = new System.Drawing.Point(3, 465);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(200, 30);
+            this.btnLogout.Size = new System.Drawing.Size(200, 36);
             this.btnLogout.TabIndex = 6;
             this.btnLogout.Text = "Logout";
             this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -287,14 +252,15 @@
             // 
             this.btnProfile.BackColor = System.Drawing.Color.DimGray;
             this.btnProfile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnProfile.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnProfile.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
             this.btnProfile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnProfile.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnProfile.ForeColor = System.Drawing.Color.White;
             this.btnProfile.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnProfile.Location = new System.Drawing.Point(0, 340);
+            this.btnProfile.Location = new System.Drawing.Point(3, 45);
             this.btnProfile.Name = "btnProfile";
-            this.btnProfile.Size = new System.Drawing.Size(200, 30);
+            this.btnProfile.Size = new System.Drawing.Size(200, 36);
             this.btnProfile.TabIndex = 5;
             this.btnProfile.Text = "Profile";
             this.btnProfile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -308,7 +274,7 @@
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelHeader.Location = new System.Drawing.Point(0, 0);
             this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Size = new System.Drawing.Size(800, 50);
+            this.panelHeader.Size = new System.Drawing.Size(1002, 50);
             this.panelHeader.TabIndex = 0;
             // 
             // btnClose
@@ -320,7 +286,7 @@
             this.btnClose.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClose.ForeColor = System.Drawing.Color.White;
             this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnClose.Location = new System.Drawing.Point(752, 4);
+            this.btnClose.Location = new System.Drawing.Point(948, 6);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(42, 41);
             this.btnClose.TabIndex = 12;
@@ -329,34 +295,56 @@
             // 
             // panelMain
             // 
+            this.panelMain.Controls.Add(this.flowLayoutPanel1);
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelMain.Location = new System.Drawing.Point(200, 50);
+            this.panelMain.Location = new System.Drawing.Point(0, 50);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(600, 400);
+            this.panelMain.Size = new System.Drawing.Size(1002, 547);
             this.panelMain.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.DimGray;
+            this.flowLayoutPanel1.Controls.Add(this.btnHome);
+            this.flowLayoutPanel1.Controls.Add(this.btnProfile);
+            this.flowLayoutPanel1.Controls.Add(this.btnReviews);
+            this.flowLayoutPanel1.Controls.Add(this.btnManageProducts);
+            this.flowLayoutPanel1.Controls.Add(this.btnMyOrders);
+            this.flowLayoutPanel1.Controls.Add(this.btnManageCustomers);
+            this.flowLayoutPanel1.Controls.Add(this.btnCart);
+            this.flowLayoutPanel1.Controls.Add(this.btnManageOrders);
+            this.flowLayoutPanel1.Controls.Add(this.btnBrowseProducts);
+            this.flowLayoutPanel1.Controls.Add(this.btnReports);
+            this.flowLayoutPanel1.Controls.Add(this.btnDashboard);
+            this.flowLayoutPanel1.Controls.Add(this.btnLogout);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(206, 547);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.WrapContents = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1002, 597);
             this.Controls.Add(this.panelMain);
-            this.Controls.Add(this.panelSidebar);
             this.Controls.Add(this.panelHeader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.Text = "Form1";
-            this.panelSidebar.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panelHeader.ResumeLayout(false);
+            this.panelMain.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panelSidebar;
         private System.Windows.Forms.Panel panelHeader;
         private System.Windows.Forms.Panel panelMain;
         private System.Windows.Forms.Button btnHome;
@@ -371,8 +359,8 @@
         private System.Windows.Forms.Button btnManageOrders;
         private System.Windows.Forms.Button btnManageCustomers;
         private System.Windows.Forms.Button btnManageProducts;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
 
